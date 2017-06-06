9404cf7ffeeec03964f651b426fca8b897d92687
Samantha Capps <samantha@wireless-v47-press00130.dhcp.unc.edu>
2017-06-05 21:06:46 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+	res.send("./views/layout.html");
+});
+
+app.listen(port, function() {
+	console.log("App listening on PORT " + port);
+});
