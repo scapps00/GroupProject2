4380b7585f7b072a4ba7748a3de5722d0ac26783
Samantha Capps <samantha@Cheez.local>
2017-06-06 20:05:12 -0400

    you're in

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "/views/layout.html"));
 });
 
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/yourein.html");
+  });
+
 app.listen(port, function() {
 	console.log("App listening on PORT " + port);
 });
